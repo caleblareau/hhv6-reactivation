5d4032bc0b9e75c7de297d3e3cde51e5fc55a07a
caleblareau <clareau@stanford.edu>
2021-06-24 00:09:27 -0700

    misc analyses

--- miseq_23june2021/code/01_analysis.R
+++ miseq_23june2021/code/01_analysis.R
@@ -1,2 +1,16 @@
 library(data.table)
+library(stringr)
+library(Seurat)
+library(dplyr)
 
+# Import data
+high <- fread("../data/ID34high_HHV6b.kb.txt") %>% filter(V3 != 120)
+txt <- data.frame(fread("../data/transcripts.txt", header = FALSE))
+mat <- Read10X_h5("../data/ID34high.filtered_feature_bc_matrix.h5")
+umi_vec <- colSums(mat)
+ngenes_vec <- colSums(mat > 0)
+ddf <- data.frame(table(paste0(high[[1]], "-1")))
+ddf$ngenes <- ngenes_vec[as.character(ddf$Var1)]; ddf$ngenes <- ifelse(is.na(ddf$ngenes), 0, ddf$ngenes)
+ddf$numis <- umi_vec[as.character(ddf$Var1)]; ddf$numis <- ifelse(is.na(ddf$numis), 0, ddf$numis)
+
+ddf %>% arrange(desc(Freq)) %>% head(20)
